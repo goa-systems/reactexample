5ec14e10932d334ec44c229f660be663ef822957
Andreas Gottardi <andreas@goa.systems>
2026-04-01 13:54:03 +0200

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+import org.gradle.api.tasks.Exec
+import org.gradle.kotlin.dsl.register
+import org.gradle.api.tasks.Delete
+import org.gradle.api.tasks.Copy
+
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.5"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "goa.systems"
+version = "0.0.1"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-webservices")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webservices-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.register<Exec>("reactRun") {
+	group = "build"
+	description = "Runs the React dev server (npm run dev)"
+	workingDir = file("src/main/react")
+	commandLine("npm", "run", "dev")
+}
+
+
+val npmBuild by tasks.register<Exec>("npmBuild") {
+    workingDir = file("src/main/react")
+    commandLine("npm", "run", "build")
+}
+
+val cleanStatic by tasks.register<Delete>("cleanStatic") {
+    delete("src/main/resources/static")
+}
+
+val copyFrontend by tasks.register<Copy>("copyFrontend") {
+    dependsOn(npmBuild, cleanStatic)
+
+    from("src/main/react/dist")
+    into("src/main/resources/static")
+}
+
+tasks.register("buildFrontend") {
+    dependsOn(copyFrontend)
+}
+
+tasks.named("processResources") {
+    dependsOn("buildFrontend")
+}
---
+++ src/main/java/goa/systems/reactexample/ReactexampleApplication.java
@@ -0,0 +1,13 @@
+package goa.systems.reactexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactexampleApplication.class, args);
+	}
+
+}
