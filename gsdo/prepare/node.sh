97bd7f14988326adb3efb289ee99e806db3527f5
Andreas Gottardi <andreas@goa.systems>
2026-04-09 18:59:13 +0200

    Splitting values.

--- gsdo/prepare/node.sh
+++ gsdo/prepare/node.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
-APP_DIR="${HOME}/.local/programs/node"
+BASE_DIR="${HOME}/.local/programs/node"
 NODE_PACKAGE=node-v${NODEVERSION}-linux-x64
-APP_DIR="${APP_DIR}/${NODE_PACKAGE}"
+APP_DIR="${BASE_DIR}/${NODE_PACKAGE}"
 if [ ! -d "${APP_DIR}" ]
 then
     wget -O "${HOME}/node.tar.gz" -q "https://nodejs.org/dist/v${NODEVERSION}/${NODE_PACKAGE}.tar.gz"
-    tar -x -f "${HOME}/node.tar.gz" -C "${APP_DIR}/.."
+    tar -x -f "${HOME}/node.tar.gz" -C "${BASE_DIR}"
     rm "${HOME}/node.tar.gz"
 else
     echo "NodeJS already installed."
