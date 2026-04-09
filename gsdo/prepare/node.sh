2ede92d0bdc2d11a2ab1622085f5144c172630f7
Andreas Gottardi <andreas@goa.systems>
2026-04-09 18:56:26 +0200

    Fixing variable name.

--- gsdo/prepare/node.sh
+++ gsdo/prepare/node.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 APP_DIR="${HOME}/.local/programs/node"
-NODE_PACKAGE=node-v${NODE_VERSION}-linux-x64
+NODE_PACKAGE=node-v${NODEVERSION}-linux-x64
 APP_DIR="${APP_DIR}/${NODE_PACKAGE}"
 if [ ! -d "${APP_DIR}" ]
 then
     mkdir -p "$APP_DIR"
-    wget -O "${HOME}/node.tar.gz" -q "https://nodejs.org/dist/v${NODE_VERSION}/${NODE_PACKAGE}.tar.gz"
+    wget -O "${HOME}/node.tar.gz" -q "https://nodejs.org/dist/v${NODEVERSION}/${NODE_PACKAGE}.tar.gz"
     tar -x -f "${HOME}/node.tar.gz" -C "${APP_DIR}"
     rm "${HOME}/node.tar.gz"
 else
