ff127d1374873e9e633a3832e6123ae8add4729c
Andreas Gottardi <andreas@goa.systems>
2026-04-13 20:02:32 +0200

    Updating README.

--- README.md
+++ README.md
@@ -4,12 +4,20 @@
 
 ### Node
 
-The project requires node installed and the commands `node`, `npm` and `npx` available in the PATH.
+The project requires [NodeJS](https://nodejs.org) installed and the commands `node`, `npm` and `npx` available in the PATH.
 
 ### Prepare frontend
 
-Go to `src/main/react` and run `npm install`.
+To install [React](https://react.dev) dependencies go to `src/main/react` and run `npm install`.
+
+### Run frontend development server
+
+Go to `src/main/react` and run `npm run dev`.
+
+### Build frontend
+
+Go to `src/main/react` and run `npm run build`.
 
 ### Build project
 
-Go to the root folder and run `./gradlew build`.
+Go to the root folder and run `./gradlew integrateFrontend build`.
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,70 +4,45 @@ import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.Copy
 
 plugins {
-	java
-	id("org.springframework.boot") version "4.0.5"
-	id("io.spring.dependency-management") version "1.1.7"
+  java
+  id("org.springframework.boot") version "4.0.5"
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "goa.systems"
 version = "0.0.1"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
-	targetCompatibility = JavaVersion.VERSION_21
+  sourceCompatibility = JavaVersion.VERSION_21
+  targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	implementation("org.springframework.boot:spring-boot-starter-webservices")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webservices-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  implementation("org.springframework.boot:spring-boot-starter-webmvc")
+  implementation("org.springframework.boot:spring-boot-starter-webservices")
+  developmentOnly("org.springframework.boot:spring-boot-devtools")
+  testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-webservices-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-}
-
-tasks.register<Exec>("reactRun") {
-	group = "build"
-	description = "Runs the React dev server (npm run dev)"
-	workingDir = file("src/main/react")
-	commandLine("npm", "run", "dev")
+  useJUnitPlatform()
 }
 
 val cleanStatic by tasks.register<Delete>("cleanStatic") {
-	delete("src/main/resources/static")
+  delete("src/main/resources/static")
 }
 
-val npmInstall by tasks.register<Exec>("npmInstall") {
-  group = "prepare"
-  description = "Install front end dependencies."
-  workingDir = file("src/main/react")
-  commandLine("npm", "install")
-}
-
-val npmBuild by tasks.register<Exec>("npmBuild") {
+val buildFrontend by tasks.register<Copy>("integrateFrontend") {
   group = "build"
-  description = "Build front end."
-  dependsOn(npmInstall)
-  workingDir = file("src/main/react")
-  commandLine("npm", "run", "build")
-}
-
-val buildFrontend by tasks.register<Copy>("buildFrontend") {
-	group = "build"
-	description = "Build frontend"
-	
-	dependsOn(npmBuild)
-
-	from("src/main/react/dist")
-	into("src/main/resources/static")
+  description = "Build frontend"
+  from("src/main/react/dist")
+  into("src/main/resources/static")
 }
 
 tasks.processResources {
