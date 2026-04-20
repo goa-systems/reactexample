24053c5b072017f0735b67f22e88be933b811779
Andreas Gottardi <andreas@goa.systems>
2026-04-20 22:20:07 +0200

    Switching to alias for scp.

--- gsdo/deploy/download.sh
+++ gsdo/deploy/download.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Define "scp -4" (IPv4) as alias for "scp". Required for this project.
+alias scp="scp -4"
+
 KEYFILE="${HOME}/.ssh/$(uuidgen)"
 KHFILE="${HOME}/.ssh/$(uuidgen)"
 
 echo "${SSH_SVCDEPLOY_HETZNER}" > "${KEYFILE}"
 ssh-keyscan -4 "${DISTSERVER}" 1> "${KHFILE}" 2> /dev/null
 chmod 600 "${KEYFILE}"
-scp -4 -i "${KEYFILE}" -o UserKnownHostsFile="${KHFILE}" "${DISTUSER}"@"${DISTSERVER}":"${DISTNAME}-${VERSION}.jar" "${HOME}/${DISTNAME}-${VERSION}.jar"
+scp -i "${KEYFILE}" -o UserKnownHostsFile="${KHFILE}" "${DISTUSER}"@"${DISTSERVER}":"${DISTNAME}-${VERSION}.jar" "${HOME}/${DISTNAME}-${VERSION}.jar"
 rm "${KHFILE}"
 rm "${KEYFILE}"
