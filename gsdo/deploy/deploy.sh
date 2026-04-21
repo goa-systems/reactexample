1566f41cacb6d75281aea09af234390a9892ce4b
Andreas Gottardi <andreas@goa.systems>
2026-04-21 19:29:01 +0200

    Fixin quotes.

--- gsdo/deploy/deploy.sh
+++ gsdo/deploy/deploy.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 APP_DIR="${HOME}/.local/programs/goasystems/reactexample"
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] - Deploying application in \"${PWD}\". [${GITHUB_SHA}]"
 ls -al "${APP_DIR}/${DISTNAME}-${VERSION}.jar"
-"${JAVA_HOME}/bin/java --version"
+"${JAVA_HOME}/bin/java" --version
