92b28965b3a6c9ac38c874d6baeed462ea3167ee
Andreas Gottardi <andreas@goa.systems>
2026-04-21 18:20:58 +0200

    Fixing JRE install script.

--- gsdo/prepare/jre.sh
+++ gsdo/prepare/jre.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 APP_DIR="${HOME}/.local/programs/java"
+if [ ! -d "${APP_DIR}" ] then; mkdir -p "$APP_DIR"; fi
 JAVA_PACKAGE=zulu${ZULUVERSION}-ca-fx-jre${JAVAVERSION}-linux_x64
 APP_DIR="${APP_DIR}/${JAVA_PACKAGE}"
 if [ ! -d "${APP_DIR}" ]
 then
-    mkdir -p "$APP_DIR"
-    wget -O "${HOME}/java.tar.gz" -q "https://cdn.azul.com/zulu/bin/${JAVA_PACKAGE}.tar.gz"
-    tar -x -f "${HOME}/java.tar.gz" -C "${APP_DIR}"
+    PKG="/tmp/$(uuidgen).tar.gz"
+    wget -4 -O "${PKG}" -q "https://cdn.azul.com/zulu/bin/${JAVA_PACKAGE}.tar.gz"
+    tar -x -f "${PKG}" -C "${APP_DIR}"
     rm "${HOME}/java.tar.gz"
 else
     echo "Java already installed."
