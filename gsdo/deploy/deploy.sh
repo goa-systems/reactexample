803f04bdeeb676373bfb2353fcefa4a46d9511d8
Andreas Gottardi <andreas@goa.systems>
2026-04-21 19:37:58 +0200

    Adding startup logic.

--- gsdo/deploy/deploy.sh
+++ gsdo/deploy/deploy.sh
@@ -3,5 +3,10 @@ set -euo pipefail
 APP_DIR="${HOME}/.local/programs/goasystems/reactexample"
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] - Deploying application in \"${PWD}\". [${GITHUB_SHA}]"
 ls -al "${APP_DIR}/${DISTNAME}-${VERSION}.jar"
+echo "server.port=${SERVER_PORT}" > "${APP_DIR}/application.properties"
 PATH="${JAVA_HOME}/bin:${PATH}"
-java --version
+OWD="${PWD}"
+cd ${APP_DIR}
+java -jar "${APP_DIR}/${DISTNAME}-${VERSION}.jar" &
+echo $! > "${APP_DIR}/run.pid"
+cd "${OWD}"
