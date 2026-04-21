87a8b742bcbc89f69adcad804eaa1fc837289dca
Andreas Gottardi <andreas@goa.systems>
2026-04-21 19:25:34 +0200

    Adding debug code.

--- gsdo/deploy/deploy.sh
+++ gsdo/deploy/deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+APP_DIR="${HOME}/.local/programs/goasystems/reactexample"
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] - Deploying application in \"${PWD}\". [${GITHUB_SHA}]"
-ls -al "${HOME}/${DISTNAME}-${VERSION}.jar"
+ls -al "${APP_DIR}/${DISTNAME}-${VERSION}.jar"
+"${JAVA_HOME}/bin/java --version"
--- gsdo/deploy/download.sh
+++ gsdo/deploy/download.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+APP_DIR="${HOME}/.local/programs/goasystems/reactexample"
+if [ ! -d "${APP_DIR}" ]; then mkdir -p "${APP_DIR}"; fi
+
 KEYFILE="${HOME}/.ssh/$(uuidgen)"
 KHFILE="${HOME}/.ssh/$(uuidgen)"
 
@@ -8,7 +11,7 @@ echo "${SSH_SVCDEPLOY_HETZNER}" > "${KEYFILE}"
 ssh-keyscan -4 "${DISTSERVER}" 1> "${KHFILE}" 2> /dev/null
 chmod 600 "${KEYFILE}"
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Starting download."
-scp -4 -i "${KEYFILE}" -o UserKnownHostsFile="${KHFILE}" "${DISTUSER}"@"${DISTSERVER}":"${DISTNAME}-${VERSION}.jar" "${HOME}/${DISTNAME}-${VERSION}.jar"
+scp -4 -i "${KEYFILE}" -o UserKnownHostsFile="${KHFILE}" "${DISTUSER}"@"${DISTSERVER}":"${DISTNAME}-${VERSION}.jar" "${APP_DIR}/${DISTNAME}-${VERSION}.jar"
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Download finished."
 rm "${KHFILE}"
 rm "${KEYFILE}"
