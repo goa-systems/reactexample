3798d56584ad2e5f7541c3083ed074bd21202501
Andreas Gottardi <andreas@goa.systems>
2026-04-21 18:24:25 +0200

    Fixing paths.

--- gsdo/prepare/jre.sh
+++ gsdo/prepare/jre.sh
@@ -3,13 +3,12 @@ set -euo pipefail
 APP_DIR="${HOME}/.local/programs/java"
 if [ ! -d "${APP_DIR}" ]; then mkdir -p "$APP_DIR"; fi
 JAVA_PACKAGE=zulu${ZULUVERSION}-ca-fx-jre${JAVAVERSION}-linux_x64
-APP_DIR="${APP_DIR}/${JAVA_PACKAGE}"
 if [ ! -d "${APP_DIR}" ]
 then
     PKG="/tmp/$(uuidgen).tar.gz"
     wget -4 -O "${PKG}" -q "https://cdn.azul.com/zulu/bin/${JAVA_PACKAGE}.tar.gz"
     tar -x -f "${PKG}" -C "${APP_DIR}"
-    rm "${HOME}/java.tar.gz"
+    rm "${PKG}"
 else
     echo "Java already installed."
 fi
