ebb596fa2ec67f5b9c4266b94e9cf7ef356fc465
Andreas Gottardi <andreas@goa.systems>
2026-04-21 21:46:12 +0200

    Switching to SystemD service.

--- gsdo/deploy/deploy.sh
+++ gsdo/deploy/deploy.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
+systemctl stop goa.systems.reactexample.service
 APP_DIR="${HOME}/.local/programs/goasystems/reactexample"
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] - Deploying application in \"${PWD}\". [${GITHUB_SHA}]"
-ls -al "${APP_DIR}/${DISTNAME}-${VERSION}.jar"
 echo "server.port=${SERVERPORT}" > "${APP_DIR}/application.properties"
-PATH="${JAVA_HOME}/bin:${PATH}"
-OWD="${PWD}"
-cd ${APP_DIR}
-java -jar "${APP_DIR}/${DISTNAME}-${VERSION}.jar" >/dev/null 2>&1 &
-PID=$!
-ps aux | grep java | grep react
-echo "Disowning ${PID}"
-disown %-
-echo $PID > "${APP_DIR}/run.pid"
-cd "${OWD}"
+systemctl start goa.systems.reactexample.service
